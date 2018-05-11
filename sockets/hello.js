1ba291439b7362bdac9938620ed5d7e606422918
juho <rjduakcjs@gmail.com>
2018-05-11 12:31:11 +0900

    small changes, added nodemon for dev

--- sockets/hello.js
+++ sockets/hello.js
@@ -1,8 +1,15 @@
 module.exports = function (socket) {
 
   socket.on("hello", (data) => {
-    console.log(data);
-    socket.emit("serverHello", `Hi this is server + ${new Date()}`);
+    console.log(`${data.greetings} : ${data.count}`);
+    socket.emit("serverHello", {
+      msg:`Hi this is server + ${new Date()}`,
+      count: data.count,
+    });
+    socket.broadcast.emit("serverHello", {
+      msg:`Hi this is server + ${new Date()}`,
+      count: data.count,
+    });
   });
 
 }
